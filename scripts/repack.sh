168977f2015c6fa17ca593a22c2a92a013414f29
Rick <ruanpaiolinho060809@gmail.com>
2026-05-28 01:42:53 -0300

    Update repack.sh

--- scripts/repack.sh
+++ scripts/repack.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/env bash
 
+set -e
+
+echo "Baixando magiskboot..."
+
+git clone --depth=1 https://github.com/topjohnwu/Magisk.git magisk
+cp magisk/tools/magiskboot .
+
+chmod +x magiskboot
+
+echo "Extraindo boot.img..."
+
+./magiskboot unpack boot.img
+
+echo "Substituindo kernel..."
+
+if [ -f kernel/out/arch/arm64/boot/Image.gz-dtb ]; then
+    cp kernel/out/arch/arm64/boot/Image.gz-dtb kernel
+elif [ -f kernel/out/arch/arm64/boot/Image.gz ]; then
+    cp kernel/out/arch/arm64/boot/Image.gz kernel
+else
+    cp kernel/out/arch/arm64/boot/Image kernel
+fi
+
+echo "Reempacotando..."
+
+./magiskboot repack boot.img
+
+mv new-boot.img droidspaces_boot.img
+
+echo "PRONTO ✅"
