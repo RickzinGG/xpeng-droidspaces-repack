a5990c648b99fb094e64e1fc91436d1759064279
Rick <ruanpaiolinho060809@gmail.com>
2026-05-28 01:42:30 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,45 @@
+#!/usr/bin/env bash
 
+set -e
+
+cd kernel
+
+export ARCH=arm64
+export SUBARCH=arm64
+
+export LLVM=1
+export LLVM_IAS=1
+
+export PATH="$(pwd)/../clang/bin:$PATH"
+
+export CC=clang
+export LD=ld.lld
+export AR=llvm-ar
+export NM=llvm-nm
+export OBJCOPY=llvm-objcopy
+export OBJDUMP=llvm-objdump
+export STRIP=llvm-strip
+
+mkdir -p out
+
+DEFCONFIG=$(find arch/arm64/configs -name "*xpeng*defconfig" | head -n 1)
+
+if [ -z "$DEFCONFIG" ]; then
+  echo "Defconfig não encontrado"
+  exit 1
+fi
+
+DEFCONFIG_NAME=$(basename $DEFCONFIG)
+
+echo "Usando: $DEFCONFIG_NAME"
+
+make O=out $DEFCONFIG_NAME
+make O=out olddefconfig
+
+make -j$(nproc --all) \
+    O=out \
+    CC=clang \
+    LLVM=1 \
+    LLVM_IAS=1 \
+    KCFLAGS="-Wno-error" \
+    KBUILD_MODPOST_WARN=1
