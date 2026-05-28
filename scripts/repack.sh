0e8a9fd3310e0f46ccd0ad1d63d224e8df229580
Rick <ruanpaiolinho060809@gmail.com>
2026-05-28 13:14:10 -0300

    Update repack.sh

--- scripts/repack.sh
+++ scripts/repack.sh
@@ -2,15 +2,23 @@
 
 set -e
 
-echo "Baixando magiskboot compatível..."
+echo "Clonando Magisk..."
 
-# 🔥 binário já pronto e compatível com GitHub runner
-curl -L -o magiskboot https://raw.githubusercontent.com/affggh/magiskboot_builds/main/magiskboot
+git clone --depth=1 https://github.com/topjohnwu/Magisk.git magisk
 
+cd magisk/native
+
+echo "Compilando magiskboot..."
+
+make magiskboot
+
+cd ../../
+
+cp magisk/native/out/magiskboot magiskboot
 chmod +x magiskboot
 
 echo "Testando magiskboot..."
-./magiskboot --help || { echo "magiskboot quebrado"; exit 1; }
+./magiskboot --help || { echo "Erro no magiskboot"; exit 1; }
 
 echo "Extraindo boot.img..."
 ./magiskboot unpack boot.img
